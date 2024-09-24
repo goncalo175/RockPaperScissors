7790fce56c569d16dd9d12aa9e66ed252292a6e6
Goncalo Lourenco (dacostag) <dacostag@coventry.ac.uk>
2024-09-24 20:55:48 +0100

    Created main screen clickable items (start,instructions and exit)

--- main.py
+++ main.py
@@ -15,11 +15,73 @@ clock = pygame.time.Clock()
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
+SKYBLUE = (135, 206, 250)
+TEAL = (0, 128, 128)
+LIGHTGRAY = (211, 211, 211)
 
 # Define font
-fontTitle = pygame.font.Font(None, 74)  # None uses default font, 74 is the font size
-fontSmall = pygame.font.Font(None, 34)
+fontGameTitle = pygame.font.Font(None, 74)  # None uses default font, 74 is the font size
+fontMenuItems = pygame.font.Font(None, 34)
 
+
+# Define actions for buttons
+def start_game():
+    running = True
+    while running:
+        screen.fill(BLACK)
+        draw_text('Game Running... Press ESC to return to Menu', fontMenuItems, WHITE, screen, 400, 300)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return 'menu'  # Return to the menu
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+def show_instructions():
+    running = True
+    while running:
+        screen.fill(BLACK)
+        draw_text("Instructions:", fontGameTitle, WHITE, screen, screen.get_width() // 2, 100)
+        draw_text("In Rock, Paper, Scissors, each player simultaneously chooses one of three options:",
+                  fontMenuItems, WHITE, screen, screen.get_width() // 2, 250)
+        draw_text("Rock, Paper, or Scissors. Rock crushes Scissors, Scissors cut Paper, and Paper covers Rock.",
+                  fontMenuItems, WHITE, screen, screen.get_width() // 2, 300)
+        draw_text("If one player's choice beats the other's, that player wins the round; if both players choose the "
+                  "same", fontMenuItems, WHITE, screen, screen.get_width() // 2, 330)
+        draw_text("option, it's a draw. Continue playing until a player wins or you decide to stop.",
+                  fontMenuItems, WHITE, screen, screen.get_width() // 2, 360)
+
+        draw_text('Press ESC to return to Menu', fontMenuItems, WHITE, screen, screen.get_width() // 2, 500)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return 'menu'  # Return to the menu
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+def exit_game():
+    pygame.quit()
+    exit()
+
+# Define main screen button properties
+button_color = TEAL # color
+hover_color = LIGHTGRAY  # Red when hovered
+# (screen_width - rect_width) // 2   == formula to get rect in the middle of the screen (X axis)
+buttons = [
+    {"rect": pygame.Rect((screen.get_width() - 200) // 2, 250, 200, 60), "text": "Start!", "action": start_game},
+    {"rect": pygame.Rect((screen.get_width() - 200) // 2, 350, 200, 60), "text": "Instructions", "action": show_instructions},
+    {"rect": pygame.Rect((screen.get_width() - 200) // 2, 450, 200, 60), "text": "Exit", "action": exit_game}
+]
 
 def draw_text(text, font, color, surface, x, y):
     textobj = font.render(text, True, color)
@@ -27,15 +89,35 @@ def draw_text(text, font, color, surface, x, y):
     surface.blit(textobj, textrect)
 
 
+def draw_button(surface, button_color, rect, text):
+    pygame.draw.rect(screen, button_color, rect)  # Draw button
+    text_surface = fontMenuItems.render(text, True, (255, 255, 255))  # Button label
+    text_rect = text_surface.get_rect(center=rect.center)
+    surface.blit(text_surface, text_rect)
+
+
 
 def main_menu():
     while True:
         screen.fill(BLUE)  # Fill the background color
+        mouse_pos = pygame.mouse.get_pos()  # Get current mouse position
+        mouse_pressed = pygame.mouse.get_pressed()  # Get mouse button press status
+
         # Draw menu options
-        draw_text('Welcome to Rock, Paper Scissors - Deluxe', fontTitle, WHITE, screen, screen.get_width() // 2, 100)
-        draw_text('Press 1 to Start', fontSmall, WHITE, screen, screen.get_width() // 2, 250)
-        draw_text('Press 2 for Instructions', fontSmall, WHITE, screen, screen.get_width() // 2, 350)
-        draw_text('Press ESC to Quit', fontSmall, WHITE, screen, screen.get_width() // 2, 450)
+        draw_text('Welcome to Rock, Paper Scissors - Deluxe', fontGameTitle, WHITE, screen, screen.get_width() // 2, 100)
+
+        # Iterate through buttons and handle drawing and interaction
+        for button in buttons:
+            rect = button["rect"]
+            text = button["text"]
+            action = button["action"]
+            if rect.collidepoint(mouse_pos):
+                draw_button(screen, hover_color, rect, text)
+                if mouse_pressed[0]:  # Left mouse button is pressed
+                    action()
+
+            else:
+                draw_button(screen, button_color, rect, text)
 
         # Check for events
         for event in pygame.event.get():
@@ -43,8 +125,7 @@ def main_menu():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:  # Start game option
-                    return 'start'
+
                 if event.key == pygame.K_2:  # Instructions option
                     return 'instructions'
                 if event.key == pygame.K_ESCAPE:  # Quit option
@@ -55,50 +136,6 @@ def main_menu():
         pygame.display.flip()
 
         # Cap frame rate
-        clock.tick(60)
-
-
-def game_loop():
-    running = True
-    while running:
-        screen.fill(BLACK)
-        draw_text('Game Running... Press ESC to return to Menu', fontSmall, WHITE, screen, 400, 300)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    return 'menu'  # Return to the menu
-
-        pygame.display.flip()
-        clock.tick(60)
-
-
-def instructions_screen():
-    running = True
-    while running:
-        screen.fill(BLACK)
-        draw_text("Instructions:", fontTitle, WHITE, screen, screen.get_width() // 2, 100 )
-        draw_text("In Rock, Paper, Scissors, each player simultaneously chooses one of three options:",
-                  fontSmall, WHITE, screen, screen.get_width() // 2, 250)
-        draw_text("Rock, Paper, or Scissors. Rock crushes Scissors, Scissors cut Paper, and Paper covers Rock.",
-                  fontSmall, WHITE, screen, screen.get_width() // 2, 300)
-        draw_text("If one player's choice beats the other's, that player wins the round; if both players choose the "
-                  "same", fontSmall, WHITE, screen, screen.get_width() // 2, 330)
-        draw_text("option, it's a draw. Continue playing until a player wins or you decide to stop.",
-                  fontSmall, WHITE, screen, screen.get_width() // 2, 360)
-
-        draw_text('Press ESC to return to Menu', fontSmall, WHITE, screen, screen.get_width() // 2, 500)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    return 'menu'  # Return to the menu
-
-        pygame.display.flip()
         clock.tick(60)
 
 
@@ -122,9 +159,9 @@ while True:
     choice = main_menu()
 
     if choice == 'start':
-        game_loop()
+        start_game()
     elif choice == 'instructions':
-        instructions_screen()
+        show_instructions()
 
 
 
